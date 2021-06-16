5b9191900984bb23bf19468d25b58298e889ec8a
Rahul <rahuljadav2121@gmail.com>
2021-06-16 11:43:45 +0530

    client added

--- routes/auth.js
+++ routes/auth.js
@@ -9,12 +9,12 @@ router.get('/google',passport.authenticate('google',{scope : ['profile']}))
 
 
 router.get('/google/callback',passport.authenticate('google',{failureRedirect : '/'}),(req,res)=>{
-    res.redirect('http://localhost:3000/pollroom')
+    res.redirect('https://pollroom.vercel.app/pollroom')
 })
 
 router.get('/logout',(req,res)=>{
     req.logOut()
-    res.redirect('http://localhost:3000')
+    res.redirect('https://pollroom.vercel.app/')
 })
 
 module.exports = router
