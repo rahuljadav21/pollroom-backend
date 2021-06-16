2faa612e940827193da54b9829cff056173db330
Rahul <rahuljadav2121@gmail.com>
2021-06-16 17:02:00 +0530

    history erorr

--- client/src/components/Polls.js
+++ client/src/components/Polls.js
@@ -15,7 +15,7 @@ function Polls({room,polls,user}) {
           },
           })
           .then(function(response) {
-           history.go(0)
+            window.open(`https://pollroom.herokuapp.com/pollroom/room/${room._id}/poll/${pollId}`,"_self")
            
           })
           .catch(function(error) {
