3fd0203746639a67a8903e90004765ff16900d57
Rahul <rahuljadav2121@gmail.com>
2021-06-16 10:13:33 +0530

    try for deploy 4

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 
 router.get('/getuser', (req, res) => {
+    res.header('Access-Control-Allow-Origin','*')
     res.send(req.user);
   })
 module.exports = router
